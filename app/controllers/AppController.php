5d7f76aa2c8191d917c68d818553edccef51b623
IDEV <coolkim2121@gmail.com>
2020-09-17 14:10:20 +0300

    FULL COMMIT

---
+++ app/controllers/AppController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\AppModel;
+use ishop\base\Controller as parentAlias;
+
+class AppController extends parentAlias
+{
+    public $test;
+    public function __construct($route)
+    {
+        parent::__construct($route);
+        new AppModel();
+    }
+
+    public function myMethod($test)
+    {
+
+    }
+}
